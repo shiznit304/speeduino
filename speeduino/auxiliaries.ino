5a8b12ca3a42f8cc91f06b233dd498e48a7cbe08
shiznit304 <62686180+shiznit304@users.noreply.github.com>
2021-10-16 00:35:46 -0500

    boostControlMAPLoad
    commit 1b2a5771cfbf0c2fddeb5bb75b40b9a236cfa4e3
    Merge: e7895180 95b8e6ee
    Author: shiznit304 <62686180+shiznit304@users.noreply.github.com>
    Date:   Sat Aug 14 18:02:41 2021 -0500
    
        Merge branch 'noisymime:master' into boostControlMAPLoad
    
    commit e7895180e26d5dd5ce5feb2eb41f21e9c89e13df
    Author: shiznit304 <mazz304@gmail.com>
    Date:   Mon Aug 2 21:55:18 2021 -0500
    
        Update speeduino.ini
    
    commit 1fffd80bf918f83564113c07717181330bf3b015
    Merge: 71eda68b 60cbf0c6
    Author: shiznit304 <62686180+shiznit304@users.noreply.github.com>
    Date:   Mon Aug 2 21:44:58 2021 -0500
    
        Merge branch 'noisymime:master' into boostControlMAPLoad
    
    commit 71eda68be91655e0912c4bdc2dbf2009cab725bb
    Merge: 3f1a4ea2 b6147b65
    Author: shiznit304 <62686180+shiznit304@users.noreply.github.com>
    Date:   Fri Jul 23 15:10:12 2021 -0500
    
        Merge branch 'noisymime:master' into boostControlMAPLoad
    
    commit 3f1a4ea296dcce1a5e8c9daa6bf4d9db7beddb98
    Author: shiznit304 <mazz304@gmail.com>
    Date:   Mon Jul 19 15:24:59 2021 -0500
    
        Update speeduino.ini
    
    commit 644d63140b85c666b06d5cdeac2affe01492fc46
    Author: shiznit304 <mazz304@gmail.com>
    Date:   Mon Jul 19 15:17:02 2021 -0500
    
        Made MAP load source open loop only
    
        Using closed loop boost control can only use TPS
    
    commit cf1d48b86c5c4d3f67a97ac1eeb0c5440d499ded
    Merge: 9cbe09f8 b5c61270
    Author: shiznit304 <mazz304@gmail.com>
    Date:   Mon Jul 19 15:04:52 2021 -0500
    
        Merge branch 'master' of https://github.com/noisymime/speeduino into boostControlMAPLoad
    
    commit 9cbe09f8c0b67c5c87d15ca9f0dde13315a17436
    Merge: 00520552 20560e65
    Author: shiznit304 <62686180+shiznit304@users.noreply.github.com>
    Date:   Wed Jul 14 19:46:19 2021 -0500
    
        Merge branch 'noisymime:master' into boostControlMAPLoad
    
    commit 0052055254d3aae6c02bd09aed10991045af27dd
    Merge: 2fc042be dabf4d1c
    Author: shiznit304 <62686180+shiznit304@users.noreply.github.com>
    Date:   Tue Jul 6 17:38:12 2021 -0500
    
        Merge branch 'noisymime:master' into boostControlMAPLoad
    
    commit 2fc042beb08a5ca002455ab4ff02e0b4b87e4c47
    Author: shiznit304 <mazz304@gmail.com>
    Date:   Mon Jul 5 01:14:00 2021 -0500
    
        Update corrections.ino
    
    commit d507de4ad4d2ef546ae099871df7e3c329870929
    Author: shiznit304 <mazz304@gmail.com>
    Date:   Mon Jul 5 01:02:49 2021 -0500
    
        boostControlMAPLoad

--- speeduino/auxiliaries.ino
+++ speeduino/auxiliaries.ino
@@ -161,7 +161,8 @@ void boostControl()
     if(configPage4.boostType == OPEN_LOOP_BOOST)
     {
       //Open loop
-      currentStatus.boostDuty = get3DTableValue(&boostTable, currentStatus.TPS, currentStatus.RPM) * 2 * 100;
+      if(configPage6.boostLoadSource == BOOST_LOAD_TPS) { currentStatus.boostDuty = get3DTableValue(&boostTable, currentStatus.TPS, currentStatus.RPM) * 2 * 100; }
+      else { currentStatus.boostDuty = get3DTableValue(&boostTable, currentStatus.MAP, currentStatus.RPM) * 2 * 100; }
 
       if(currentStatus.boostDuty > 10000) { currentStatus.boostDuty = 10000; } //Safety check
       if(currentStatus.boostDuty == 0) { DISABLE_BOOST_TIMER(); BOOST_PIN_LOW(); } //If boost duty is 0, shut everything down
